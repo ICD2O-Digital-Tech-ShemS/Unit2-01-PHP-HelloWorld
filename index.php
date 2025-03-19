51d92f75efbbf3a53f71986a69acdf2fba6f2b6e
Shem Irekpita <Shem.irekpita@stu.ocsb.ca>
2025-03-19 15:03:52 +0000

    This is my project

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
         <link rel="icon" type="image/png" sizes="180x180" href="./Favicon/JavaScript.png">
 	</head>
 	<body>
-        <h1><? php echo "Hello, World! in PHP"; ?></h1>
-        <p><? php echo "This is my first web page using PHP";?></p>
+        <h1><?php echo "Hello, World! in PHP"; ?></h1>
+        <p><?php echo "This is my first web page using PHP";?></p>
 	</body>
 </html>
