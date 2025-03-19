a3e91f7194341d6c47e36a8fe1f786cfb7b98551
Shem Irekpita <Shem.irekpita@stu.ocsb.ca>
2025-03-19 14:42:34 +0000

    this is my code in php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello, World! in PHP</title>
+    <link rel="icon" type="image/png" sizes="180x180" href="./Favicon/javascript.png">
+	</head>
+	<body>
+        <h1><? php echo "Hello, World! in PHP"; ?></h1>
+        <p><? php echo "This is my first web page using PHP";?></p>
+	</body>
+</html>
