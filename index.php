ce3e45e50c393826fd931b3a9d4132e6f424b4d5
Shem Irekpita <Shem.irekpita@stu.ocsb.ca>
2025-03-19 14:59:08 +0000

    This is my project

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
 	<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Hello, World! in PHP</title>
-    <link rel="icon" type="image/png" sizes="180x180" href="./Favicon/javascript.png">
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Hello, World! in PHP</title>
+        <link rel="icon" type="image/png" sizes="180x180" href="./Favicon/JavaScript.png">
 	</head>
 	<body>
         <h1><? php echo "Hello, World! in PHP"; ?></h1>
